6ea99e6efd15a9325967fe888fad52cf61f11334
Thomas Remion <thomas.remion@outlook.com>
2025-08-11 23:33:11 +0100

    Try to compile

--- project3/lib/counter.ml
+++ project3/lib/counter.ml
@@ -1,5 +1,4 @@
-(* open Hardcaml.Signal *)
-(* open Hardcaml *)
+open Hardcaml
 
 module I = struct
   type 'a t = { clock : 'a } [@@deriving sexp_of, hardcaml]
@@ -8,3 +7,5 @@ end
 module O = struct
   type 'a t = { leds : 'a [@bits 6] } [@@deriving sexp_of, hardcaml]
 end
+
+let create (_i : _ I.t) = { O.leds = Signal.of_string "111111" }
