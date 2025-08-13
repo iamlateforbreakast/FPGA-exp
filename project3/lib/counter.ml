1f1ee7fc42835e740f89d7377acd0e0ba0fc9005
Thomas Remion <thomas.remion@outlook.com>
2025-08-13 14:59:07 +0100

    Improve

--- project3/lib/counter.ml
+++ project3/lib/counter.ml
@@ -5,9 +5,7 @@ open Hardcaml.Signal
 
 module I = struct
   type 'a t =
-    { clk : 'a;
-      clear : 'a;
-      incr  : 'a }
+    { clk : 'a }
   [@@deriving hardcaml]
 end
 
@@ -18,10 +16,22 @@ module O = struct
   [@@deriving hardcaml]
 end
 
+(* Frequency is 27MHz to 1/2 s wait time betwen led increment *)
+let wait_time = 13_499_999
+
 let create (i : _ I.t) =
-  { O.leds = reg_fb
-      (Reg_spec.create ~clock:i.clk ~clear:i.clear ())
-      ~enable:i.incr
-      ~width:6
-      ~f:(fun d -> d +:. 1)
+  let clk_counter = reg_fb
+                       (Reg_spec.create ~clock:i.clk ~clear:gnd ())
+                       ~enable:vdd
+                       ~width:32
+                       ~f:(fun d -> 
+                        mux2 (d ==:. wait_time) (zero 32) (d +:. 1)) in
+  let led_counter = reg_fb
+                       (Reg_spec.create ~clock:i.clk ~clear:gnd ())
+                       ~enable:vdd
+                       ~width: 6
+                       ~f:(fun d -> 
+                        mux2 (clk_counter ==:. wait_time) (d +:. 1) d) in
+  { O.leds = ~:led_counter 
   }
+ 
