f7279d08827f165c8cf05f720ccafebd15d896b3
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-08-17 23:21:46 +0100

    Update gowin_clkdiv.ml

--- project6/lib/gowin_clkdiv.ml
+++ project6/lib/gowin_clkdiv.ml
@@ -1,1 +1,22 @@
 (* gowin_clkdiv.ml *)
+open Hardcaml
+
+module I = struct
+  type 'a t =
+    { hclkin : 'a
+    ; resetn : 'a
+    } 
+  [@@deriving sexp_of, hardcaml]
+end
+
+module O = struct
+  type 'a t =
+    { clkout : 'a
+    }
+  [@@deriving sexp_of, hardcaml]
+end
+
+let create (input : _ I.t) =
+  let module Inst = Instantiation.With_interface(I)(O) in
+  Inst.create ~name:"gowin_clkdiv" input
+;;
