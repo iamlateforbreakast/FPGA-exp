4cb38cb77361ad071f5a4875fed324e3d215dc97
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-08-18 22:26:26 +0100

    Update svo_hdmi.ml

--- project6/hdmi_hardcaml/lib/svo_hdmi.ml
+++ project6/hdmi_hardcaml/lib/svo_hdmi.ml
@@ -33,9 +33,24 @@ module Make (X : Config.S) = struct
   module Tmds = Svo_tmds.Make(X)
 
   let create (scope : Scope.t) (i : _ I.t) =
-    let tcard = Tcard.hierarchical scope (Tcard.I.{ resetn=i.resetn; clk=i.clk; out_axis_tready=Signal.vdd }) in
-    let enc = Encoder.hierarchical scope (Encoder.I.{ clk=i.clk; resetn=i.resetn; out_axis_tready=i.out_axis_tready; clk_pixel=i.clk_pixel; clk_5x_pixel=i.clk_5x_pixel; locked=i.locked }) in
-    let tmds = Tmds.hierarchical scope (Tmds.I.{ clk=i.clk; resetn=i.resetn; out_axis_tready=i.out_axis_tready; clk_pixel=i.clk_pixel; clk_5x_pixel=i.clk_5x_pixel; locked=i.locked }) in
+    let tcard = Tcard.hierarchical scope (
+	  Tcard.I.{ resetn=i.resetn
+              ; clk=i.clk
+			  ; out_axis_tready=Signal.vdd }) in
+    let enc = Encoder.hierarchical scope (
+	  Encoder.I.{ clk=i.clk
+                ; resetn=i.resetn
+				; out_axis_tready=i.out_axis_tready
+	            ; clk_pixel=i.clk_pixel
+			    ; clk_5x_pixel=i.clk_5x_pixel
+	            ; locked=i.locked }) in
+    let tmds = Tmds.hierarchical scope (
+	  Tmds.I.{ clk=i.clk
+             ; resetn=i.resetn
+			 ; out_axis_tready=i.out_axis_tready
+	         ; clk_pixel=i.clk_pixel
+		     ; clk_5x_pixel=i.clk_5x_pixel
+	         ; locked=i.locked }) in
     (* Connect the outputs of the encoder to the TMDS outputs *)
     {
       O.tmds_clk_n = tcard.tmds_clk_n;
