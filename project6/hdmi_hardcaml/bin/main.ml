32801c21254305230dcc193b3fc6d977d942b307
Thomas Remion <thomas.remion@outlook.com>
2025-08-20 10:34:31 +0100

    Try to generate

--- project6/hdmi_hardcaml/bin/main.ml
+++ project6/hdmi_hardcaml/bin/main.ml
@@ -32,8 +32,6 @@ end
 
 module Hdmi = Svo_hdmi.Make(My_config)
 
-module TopCircuit = Circuit.With_interface(I)(O)
-
 let create (scope: Scope.t) (input: _ I.t)=
   let rpll = Gowin_rpll.hierarchical scope 
     (Gowin_rpll.I.{clkin = input.clkin}) in
@@ -55,19 +53,21 @@ let create (scope: Scope.t) (input: _ I.t)=
     O.tmds_d_p = hdmi.tmds_d_p;
   }
   
-  let hierarchical (scope : Scope.t) (i : Signal.t I.t) : Signal.t O.t =
-    let module H = Hierarchy.In_scope(I)(O) in
+let hierarchical (scope : Scope.t) (i : Signal.t I.t) : Signal.t O.t =
+  let module H = Hierarchy.In_scope(I)(O) in
     H.hierarchical ~scope ~name:"top" create i
 
-(*let output_mode = Rtl.Output_mode.To_file("hdmi.v")*)
-
-let () = 
+let () =
+  let module TopCircuit = Circuit.With_interface(I)(O) in
+  let scope = Scope.create ~flatten_design:true () in
+  let circuit = TopCircuit.create_exn ~name:"top" (hierarchical scope) in
+  let database = Scope.circuit_database scope in
+  (* Generate the circuit *)
   let output_dir = "verilog_out" in
   let _ = Sys.command ("mkdir -p " ^ output_dir) in
-  let scope = Scope.create ~flatten_design:false () in
-  let db = Circuit_database.create_exn ~top_level_name:"top" (module TopCircuit.I)(module TopCircuit.O) (hierarchical scope)
-  in
-    Rtl.output 
-      ~output_mode:Verilog_mode.Verilog_file_per_module
-      ~output_path:output_dir
-      db
+
+  Rtl.output
+      ~database
+      Verilog
+      ~output_mode:(In_directory output_dir)
+      circuit
