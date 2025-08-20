051b7f86309d9610582c2824e0ed7a55e939f292
Thomas Remion <thomas.remion@outlook.com>
2025-08-20 18:00:50 +0100

    Add simulation

---
+++ project6/hdmi_hardcaml/bin/simulation.ml
@@ -0,0 +1,41 @@
+(* simulation.ml *)
+
+open Hardcaml
+open Hardcaml_waveterm
+open Project6_lib
+
+module My_config = struct
+  let svo_mode = "640x480V"
+  let svo_frame_rate = 60
+  let svo_bits_per_pixel = 24
+  let svo_bits_per_red = 8
+  let svo_bits_per_green = 8
+  let svo_bits_per_blue = 8
+  let svo_bits_per_alpha = 0
+  let svo_hor_pixels = 640
+end
+
+module Hdmi = Svo_hdmi.Make(My_config)
+
+module Simulator = Cyclesim.With_interface (Hdmi.I)(Hdmi.O)
+
+let testbench n =
+  let scope = 
+    Scope.create 
+      ~auto_label_hierarchical_ports:true
+      ~flatten_design:true () in
+  let sim = 
+    Simulator.create
+      ~config:Cyclesim.Config.trace_all (Hdmi.create scope) in
+      let waves, sim = Waveform.create sim in
+
+  for _i = 0 to n do
+    Cyclesim.cycle sim
+  done;
+  waves
+
+let () =
+  let waves = testbench 15 in
+  Hardcaml_waveterm.Interactive.run ~wave_width:5 ~signal_width:30 waves
+
+     
