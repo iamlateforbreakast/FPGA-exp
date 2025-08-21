d35c3591e35ed444348032e839a72ebc83ee7d55
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-08-21 10:06:00 +0100

    Create screen.ml

---
+++ project8/spi_hardcaml/lib/screen.ml
@@ -0,0 +1,25 @@
+(* screen.ml *)
+open Hardcaml
+open Signal
+
+module type Config = Config.S
+
+module Make (X : Config.S) = struct
+
+  module I = struct
+    type 'a t =
+      { clk : 'a
+      } 
+    [@@deriving hardcaml]
+  end
+
+  module O = struct
+    type 'a t =
+      { io_sclk : 'a
+      ; io_sdin : 'a
+      ; io_cs : 'a
+      ; io_dc : 'a
+      ; io_reset : 'a
+      }
+    [@@deriving hardcaml]
+  end
