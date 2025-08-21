7df7858580d5a9a552a21dc2aeae47f415ad59bd
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-08-21 10:37:17 +0100

    Create simulation.ml

---
+++ project8/spi_hardcaml/bin/simulation.ml
@@ -0,0 +1,34 @@
+(* simulation.ml *)
+
+open Hardcaml
+open Hardcaml_waveterm
+open Project8_lib
+
+module My_config = struct
+  let file_name = "image.hex"
+end
+
+module MyScreen = Screen.Make(My_config)
+
+module Simulator = Cyclesim.With_interface (Hdmi.I)(Hdmi.O)
+
+let testbench n =
+  let scope = 
+    Scope.create 
+      ~auto_label_hierarchical_ports:true
+      ~flatten_design:true () in
+  let sim = 
+    Simulator.create
+      ~config:Cyclesim.Config.trace_all (Hdmi.create scope) in
+      let waves, sim = Waveform.create sim in
+
+  for _i = 0 to n do
+    Cyclesim.cycle sim
+  done;
+  waves
+
+let () =
+  let waves = testbench 15 in
+  Hardcaml_waveterm_interactive.run ~wave_width:5 ~signals_width:30 waves
+
+     
