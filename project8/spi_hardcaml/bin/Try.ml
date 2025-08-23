c2d419fe89e04c4bc52341f668bc685ffcfef6f7
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-08-23 12:41:30 +0100

    Create Try.ml

---
+++ project8/spi_hardcaml/bin/Try.ml
@@ -0,0 +1,16 @@
+(* Try.ml *)
+
+module Test = struct
+  module I = struct
+
+  module O = struct
+
+  let create (scope : Scope.t)(i: I.t) =
+  {
+  }
+end
+
+let () =
+  let open Test in
+  let scope = Scope.create in
+  Rt
