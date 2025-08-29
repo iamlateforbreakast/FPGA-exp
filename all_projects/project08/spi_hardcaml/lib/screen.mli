3ca806ae1b6357939bbcb76aa1a04daea1d95c84
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-08-30 00:36:15 +0100

    Update screen.mli

--- all_projects/project08/spi_hardcaml/lib/screen.mli
+++ all_projects/project08/spi_hardcaml/lib/screen.mli
@@ -1,6 +1,6 @@
 open Hardcaml
 (** A configuration for the screen module. *)
-module type Config = Config_intf.S
+module type Config = Config_intf
 
 (** The top-level screen module. *)
 module Make ( _ : Config) : sig
