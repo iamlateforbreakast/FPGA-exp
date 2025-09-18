62ac0d9c94c96a3c4c78abd8aa739ea839c52c63
Thomas Remion <thomas.remion@outlook.com>
2025-09-18 14:05:47 +0100

    Add parameter

--- all_projects/project06/hdmi_hardcaml/lib/gowin_rpll.ml
+++ all_projects/project06/hdmi_hardcaml/lib/gowin_rpll.ml
@@ -18,9 +18,16 @@ end
 
 module Circuit = Circuit.With_interface(I)(O)
 
+let parameters = 
+  List.map
+  (fun (name, value) -> Parameter.create ~name ~value)
+  [
+    "FBDIV", Parameter.Value.Int 5;  (* Feedback Divider *)
+  ]
+
 let create (_scope : Scope.t) (i : _ I.t) =
   let module Inst = Instantiation.With_interface(I)(O) in
-  Inst.create ~name:"gowin_rpll" i
+  Inst.create ~name:"gowin_rpll" ~parameters i
 
 let hierarchical (scope : Scope.t) (i : Signal.t I.t) : Signal.t O.t =
   let module H = Hierarchy.In_scope(I)(O) in
