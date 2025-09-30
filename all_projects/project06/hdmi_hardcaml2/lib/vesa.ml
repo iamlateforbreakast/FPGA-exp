2edfa013d638eef27953548de343753d0a22a934
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-09-30 12:01:49 +0100

    Update vesa.ml

--- all_projects/project06/hdmi_hardcaml2/lib/vesa.ml
+++ all_projects/project06/hdmi_hardcaml2/lib/vesa.ml
@@ -8,7 +8,7 @@ module Make (X : Config) = struct
   module I = struct
     type 'a t =
       { clock :    'a [@bits 1]  (* Need to be called clock for simulation *)
-      ; i_resetn :  'a [@bits 1]
+      ; i_reset :  'a [@bits 1]
       } 
     [@@deriving hardcaml]
   end
@@ -25,7 +25,28 @@ module Make (X : Config) = struct
   end
 
   let create (_scope: Scope.t) (i: _ I.t) : _ O.t =
-  {O.o_column = zero 11; O.o_row = zero 11; o_vsync = gnd; o_hsync = gnd; o_data_en = gnd}
+    let open Signal in
+    let reg_sync_spec = Reg_spec.create ~clock:i.clock ~clear:i_reset () in
+    let column = reg_fb reg_sync_spec 
+      ~enable:vdd 
+      ~width:11
+      ~f:(fun c -> mux2 (c <:. (X.clk_div * 2 - 1)) (c +:. 1)(zero 16)) in
+    let _dbg_counter = Signal.(counter -- "dbg_column") in
+    let row = reg_fb reg_sync_spec 
+      ~enable:vdd 
+      ~width:11 
+      ~f:(fun c -> mux2 (c <:. (X.clk_div * 2 - 1)) (c +:. 1)(zero 16)) in
+    let vsync = reg_fb reg_sync_spec 
+      ~enable:vdd 
+      ~width:1
+      ~f:(fun c -> mux2 (c <:. (X.clk_div * 2 - 1)) (c +:. 1)(zero 16)) in
+    let _dbg_counter = Signal.(counter -- "dbg_vsync") in
+    let hsync = reg_fb reg_sync_spec 
+      ~enable:vdd 
+      ~width:1
+      ~f:(fun c -> mux2 (c <:. (X.clk_div * 2 - 1)) (c +:. 1)(zero 16)) in
+    let _dbg_counter = Signal.(counter -- "dbg_hsync") in
+    {O.o_column = column; O.o_row = row; o_vsync = vsync; o_hsync = hsync; o_data_en = gnd}
   
 end
 
