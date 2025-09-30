60ad2191df304381f35baf636b3525fb0a455473
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-09-30 11:06:38 +0100

    Update vesa.ml

--- all_projects/project06/hdmi_hardcaml2/lib/vesa.ml
+++ all_projects/project06/hdmi_hardcaml2/lib/vesa.ml
@@ -1,1 +1,31 @@
+(* vesa.ml *)
+open Hardcaml
+
+module type Config = Config.S
+
+module Make (X : Config) = struct
+  
+  module I = struct
+    type 'a t =
+      { clock :    'a [@bits 1]  (* Need to be called clock for simulation *)
+      ; i_resetn :  'a [@bits 1]
+      } 
+    [@@deriving hardcaml]
+  end
+
+  module O = struct
+    type 'a t =
+      { o_column :  'a [@bits 11]
+      ; o_row :  'a [@bits 11]
+      ; o_vsync :    'a [@bits 1]
+      ; o_hsync :    'a [@bits 1]
+      ; o_data_en : 'a [@bits 1]
+      }
+    [@@deriving hardcaml]
+  end
+
+  let create (_scope: Scope.t) (i: _ I.t) : _ O.t =
+  {O.o_column = zero 11; O.o_row = zero 11; o_vsync = gnd; o_hsync = gnd; o_data_en = gnd}
+  
+end
 
