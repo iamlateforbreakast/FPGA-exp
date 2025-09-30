e02e87626175c21caef887f27032515252fa1f0d
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-09-30 14:38:06 +0100

    Update top.ml

--- all_projects/project06/hdmi_hardcaml2/lib/top.ml
+++ all_projects/project06/hdmi_hardcaml2/lib/top.ml
@@ -16,15 +16,10 @@ module Make (X : Config) = struct
     type 'a t =
       { o_tmds_clk_p :  'a [@bits 1]
       ; o_tmds_clk_n :  'a [@bits 1]
-      ; o_tmds_data_p :    'a [@bits 3]
-      ; o_tmds_data_n :    'a [@bits 3]
+      ; o_tmds_data_p : 'a [@bits 3]
+      ; o_tmds_data_n : 'a [@bits 3]
       }
     [@@deriving hardcaml]
   end
 
 end
-
-output O_tmds_clk_p;
-output O_tmds_clk_n;
-output [2:0] O_tmds_data_p;
-output [2:0] O_tmds_data_n;
