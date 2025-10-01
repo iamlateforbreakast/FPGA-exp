141943271256e82707807d49c3e03b2bee9e0ebd
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-10-01 16:17:45 +0100

    Update top.ml

--- all_projects/project06/hdmi_hardcaml2/lib/top.ml
+++ all_projects/project06/hdmi_hardcaml2/lib/top.ml
@@ -7,32 +7,38 @@ module Make (X : Config) = struct
   
   module I = struct
     type 'a t =
-      { clock :    'a [@bits 1]  (* Need to be called clock for simulation *)
+      { clock :    'a [@bits 1]  (* input sys_clk; *)
       } 
     [@@deriving hardcaml]
   end
 
   module O = struct
     type 'a t =
-      { o_tmds_clk_p :  'a [@bits 1]
-      ; o_tmds_clk_n :  'a [@bits 1]
-      ; o_tmds_data_p : 'a [@bits 3]
-      ; o_tmds_data_n : 'a [@bits 3]
+      { o_tmds_clk_p :  'a [@bits 1] (* output O_tmds_clk_p; *)
+      ; o_tmds_clk_n :  'a [@bits 1] (* output O_tmds_clk_n; *)
+      ; o_tmds_data_p : 'a [@bits 3] (* output [2:0] O_tmds_data_p; *)
+      ; o_tmds_data_n : 'a [@bits 3] (* output [2:0] O_tmds_data_n; *)
       }
     [@@deriving hardcaml]
   end
 
   module MyVesa = Vesa.Make(X)
   module MyDvi_tx = Dvi_tx.Make(X)
+  
   let create (scope: Scope.t) (i: _ I.t)=
     let open Signal in
     let open Always in
     let reg_sync_spec = Reg_spec.create ~clock:i.clock ~clear:gnd () in
+    (* reg [2:0] colors = 0; *)
     let color_r = Variable.reg ~enable:vdd reg_sync_spec ~width:8 in
     let color_g = Variable.reg ~enable:vdd reg_sync_spec ~width:8 in
     let color_b = Variable.reg ~enable:vdd reg_sync_spec ~width:8 in
+    (* gowin_rpll pll(dvi_clk, sys_clk); *)
     let rpll = Gowin_rpll.hierarchical scope 
       (Gowin_rpll.I.{clkin = i.clock}) in
+    (* CLKDIV div_by_5(.RESETN(1'b1), .HCLKIN(dvi_clk), .CLKOUT(pix_clk), .CALIB (1'b0)); *)
+    (* defparam div_by_5.DIV_MODE="5"; *)
+    (* defparam div_by_5.GSREN="false"; *)
     let clkdiv = Gowin_clkdiv.hierarchical scope 
       (Gowin_clkdiv.I.{ hclkin = rpll.clkout
                       ; resetn = vdd
