b9b140f2da0894250cea7747180a167e36b2358e
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-01-12 15:43:56 +0000

    Update ws2812.ml

--- all_projects/project04/led_hardcaml/lib/ws2812.ml
+++ all_projects/project04/led_hardcaml/lib/ws2812.ml
@@ -1,1 +1,27 @@
+(* ws2812.ml *)
+open Hardcaml
 
+module type Config = Config.S
+
+module Make (X : Config) = struct
+  
+  module I = struct
+  type 'a t = {
+    clk   : 'a;
+    reset : 'a;
+    color : 'a; [@bits 24]
+  } [@@deriving hardcaml]
+end
+
+module O = struct
+  type 'a t = {
+    data : 'a;
+  } [@@deriving hardcaml]
+end
+
+let create (_scope: Scope.t) (i: _ I.t) : _ O.t =
+  {O.data = gnd}
+
+let hierarchical (scope : Scope.t) (i : Signal.t I.t) : Signal.t O.t =
+    let module H = Hierarchy.In_scope(I)(O) in
+    H.hierarchical ~scope ~name:"ws2812" ~instance:"inst1" create i
