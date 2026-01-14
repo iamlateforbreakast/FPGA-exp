dda255bf7781f1365efb3622f8feadf8d4681869
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2026-01-14 20:31:04 +0000

    Review by AI

--- all_projects/project05/uart_hardcaml/lib/uart.ml
+++ all_projects/project05/uart_hardcaml/lib/uart.ml
@@ -1,15 +1,14 @@
 (* uart.ml *)
 open Hardcaml
 open Signal
-open Uart_tx
 
 module Make (Config : Config.S) = struct
-  open Config
-
+  (* We don't necessarily need to open Config if we use it for parameters *)
+  
   module I = struct
     type 'a t =
       { clock : 'a
-      ; clear : 'a
+      ; resetn : 'a
       }
     [@@deriving sexp_of, hardcaml]
   end
@@ -17,33 +16,34 @@ module Make (Config : Config.S) = struct
   module O = struct
     type 'a t =
       { tx_pin : 'a
-      ; rx_pin : 'a
+      ; tx_data_ready : 'a
       }
     [@@deriving sexp_of, hardcaml ~rtlmangle:true]
   end
 
-  let create
-    ~build_mode
-    scope
-    { I.clock; clear }
-    =
+  let create ~build_mode scope { I.clock; resetn; tx_data; tx_data_valid } =
     let ( -- ) = Scope.naming scope in
-    let open Always in
-    (* preprocess the scalar *)
-    let spec = Reg_spec.create ~clock () in
-    let spec_with_clear = Reg_spec.create ~clear ~clock () in
-    (* let sm = State_machine.create (module State) spec_with_clear in*)
-    
+
+    (* Instantiate the UART Transmitter sub-module *)
     let tx =
       Uart_tx.hierarchical
         ~build_mode
         scope
-        { Uart_tx.I.clock
-        ; clear
-        } in
-    { O.tx_pin = vdd; rx_pin = vdd }
+        { Uart_tx.I.
+          clock;
+          resetn;
+          tx_data;
+          tx_data_valid;
+        } 
+    in
+
+    (* Name the outputs for better RTL generation/debugging *)
+    { O.
+      tx_pin = tx.tx_pin -- "UART_TX_PIN";
+      tx_data_ready = tx.tx_data_ready -- "UART_TX_READY";
+    }
     
   let hierarchical ?instance ~build_mode scope =
     let module H = Hierarchy.In_scope (I) (O) in
-    H.hierarchical ?instance ~name:"top" ~scope (create ~build_mode)
+    H.hierarchical ?instance ~name:"uart_top" ~scope (create ~build_mode)
 end
