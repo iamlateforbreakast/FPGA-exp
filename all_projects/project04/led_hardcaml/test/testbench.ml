3411f86666716aec2b188edcf9071808cd131c3f
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2026-01-14 14:19:05 +0000

    Correct test

--- all_projects/project04/led_hardcaml/test/testbench.ml
+++ all_projects/project04/led_hardcaml/test/testbench.ml
@@ -17,32 +17,58 @@ module Simulator = Cyclesim.With_interface(MyWs2812.I)(MyWs2812.O)
 
 
 let testbench () =
-  let scope = Scope.create 
-      ~auto_label_hierarchical_ports:true
-      ~flatten_design:true () in
+  let scope = Scope.create ~auto_label_hierarchical_ports:true ~flatten_design:true () in
   let sim = Simulator.create (MyWs2812.create scope) in
-  let _inputs : _ MyWs2812.I.t = Cyclesim.inputs sim in
-  let outputs : _ MyWs2812.O.t = Cyclesim.outputs sim in
-  let step () =
-    let _ = Printf.printf "data=%i\n"
-      (Bits.to_int !(outputs.data));
+  let inputs = Cyclesim.inputs sim in
+  let outputs = Cyclesim.outputs sim in
 
-  let check_init_state _cycle =
-    assert (Bits.to_int !(outputs.data) = 1);
-    () in
+  let step cycle =
+    Printf.printf "cycle=%d data=%d\n" cycle (Bits.to_int !(outputs.data))
+  in
 
-  for cycle = 0 to 27 do
+  (* Initialize inputs *)
+  inputs.color := Bits.of_int ~width:24 0xFF0000;
+
+  (* Reset module *)
+  inputs.reset := Bits.vdd;
+  Cyclesim.cycle sim;
+  step 0;
+  inputs.reset := Bits.gnd;
+
+  for cycle = 1 to 26 do
     Cyclesim.cycle sim;
-    check_init_state cycle;
+    step cycle;
   done;
+  () 
+
 
 let%expect_test "ws2812" =
   testbench ();
   [%expect {|
-    data=1
-    data=1
-    data=1
-    data=1
-    data=1
-    data=1
+    cycle=1 data=0
+    cycle=2 data=0
+    cycle=3 data=0
+    cycle=4 data=0
+    cycle=5 data=0
+    cycle=6 data=0
+    cycle=7 data=0
+    cycle=8 data=0 
+    cycle=9 data=0
+    cycle=10 data=0
+    cycle=11 data=0
+    cycle=12 data=0
+    cycle=13 data=0
+    cycle=14 data=0
+    cycle=15 data=0
+    cycle=16 data=0
+    cycle=17 data=0
+    cycle=18 data=0
+    cycle=19 data=0
+    cycle=20 data=0
+    cycle=21 data=0
+    cycle=22 data=0
+    cycle=23 data=0
+    cycle=24 data=0
+    cycle=25 data=0
+    cycle=26 data=0
   |}]
