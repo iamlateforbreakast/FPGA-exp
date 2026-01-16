292d508fcaca5ea39c821c9e60bc037d03c083fd
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-01-16 17:18:13 +0000

    Update top.ml

--- all_projects/project05/uart_hardcaml/lib/top.ml
+++ all_projects/project05/uart_hardcaml/lib/top.ml
@@ -21,7 +21,7 @@ module Make (X : Config.S) = struct
       }
     [@@deriving hardcaml]
   end
-
+  
   let message_rom ~index =
     let open Signal in
     let chars = X.message |> String.to_seq |> List.of_seq in
@@ -29,6 +29,9 @@ module Make (X : Config.S) = struct
     mux index rom
 
   let create (_scope : Scope.t) (_input : Signal.t I.t) : Signal.t O.t =
+    let sync_spec = Reg_spec.create ~clock ~reset () in
+
+	(* Constants *)
     let _wait_time  = if (X.is_simulation = false) then 1
 	                   else (X.cycle_period / X.clk_fre) - 1 in
     { O.tx_pin = vdd; tx_valid = vdd }
