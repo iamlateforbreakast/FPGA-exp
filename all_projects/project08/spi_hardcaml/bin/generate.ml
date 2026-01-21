bcb21423a17d0a533e405a305fbc52e79f62ede9
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-01-21 14:09:03 +0000

    Update command list

--- all_projects/project08/spi_hardcaml/bin/generate.ml
+++ all_projects/project08/spi_hardcaml/bin/generate.ml
@@ -6,7 +6,25 @@ module My_config = struct
   let file_name = "image.hex"
   let startup_wait = 10_000_000
   let clk_div = 4
-  let commands = [ 0xAE; 0x81; 0x7F; 0xA6; 0x20; 0x00; 0xDB; 0x20; 0xD9; 0x22; 0x8D; 0x14; 0xA4; 0XAF ]
+  let commands = [
+    0xAE; (* Display Off *)
+    0xD5; 0x80; (* Clock Divide *)
+    0xA8; 0x3F; (* Mux Ratio *)
+    0xD3; 0x00; (* Offset *)
+    0x40; (* Start Line *)
+    0x8D; 0x14; (* Charge Pump *)
+    0x20; 0x00; (* Memory Mode: Horizontal *)
+    0xA1; (* Segment Remap *)
+    0xC8; (* COM Output Scan Direction *)
+    0xDA; 0x12; (* COM Pins hardware config *)
+    0x81; 0xCF; (* Contrast *)
+    0xD9; 0xF1; (* Pre-charge *)
+    0xDB; 0x40; (* VCOMH Deselect *)
+    0xA4; (* Resume RAM to Display *)
+    0xA6; (* Normal Display *)
+    0xAF; (* Display On *)
+  ]
+    
   let is_simulation = false
 end
 
