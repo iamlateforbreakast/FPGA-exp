ece66228d8c91caca4dd67859f3f0e0d86ca83a7
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2026-01-22 17:57:41 +0000

    Add SPI divider

--- all_projects/project08/spi_hardcaml/lib/screen_spi.ml
+++ all_projects/project08/spi_hardcaml/lib/screen_spi.ml
@@ -19,8 +19,7 @@ module Make (X : Config.S) = struct
     type 'a t =
       { mosi : 'a
       ; sclk : 'a
-      ; cs : 'a
-      ; dc : 'a
+      ; cs :   'a
       ; ready : 'a
       }
     [@@deriving sexp_of, hardcaml]
@@ -29,6 +28,13 @@ module Make (X : Config.S) = struct
   let create (_scope : Scope.t) (input : Signal.t I.t) : Signal.t O.t =
     let spec = Reg_spec.create ~clock:input.clock ~reset:input.reset () in
   
+    (* Define a divider constant in your Config *)
+    let divider_limit = 5 in
+
+    (* Inside create function *)
+    let clk_cnt = Always.Variable.reg spec ~width:8 in
+    let clk_tick = Always.Variable.wire ~default:(zero 1) in
+
     (* Internal Registers *)
     let shift_reg = Always.Variable.reg spec ~width:8 in
     let bit_cnt   = Always.Variable.reg spec ~width:4 in
@@ -39,28 +45,37 @@ module Make (X : Config.S) = struct
     let _ = Signal.(running.value -- "running") in
     let _ = Signal.(bit_cnt.value -- "bit_cnt") in
     let _ = Signal.(shift_reg.value -- "shift_reg") in
+    let _ = (input.data_in -- "data_in") in
+    let _ = (input.data_valid -- "data_valid") in
 
     Always.(compile [
+      if_ (clk_cnt.value ==:. divider_limit - 1) [
+        clk_cnt <--. 0;
+      ] [
+        clk_cnt <-- clk_cnt.value +:. 1;
+      ];
+      clk_tick <-- (clk_cnt.value ==:. 0);
       if_ (input.data_valid &: ~:(running.value) ) [
         shift_reg <-- input.data_in;
         bit_cnt   <--. 0;
         running   <-- vdd;
+        sclk_reg  <-- gnd;
       ] [
-          (* Toggle clock and shift logic *)
-          sclk_reg <-- ~:(sclk_reg.value);
-          if_ (sclk_reg.value) [ (* Falling edge of SCLK *)
+          if_ (running.value &: clk_tick.value) [
+            (* Toggle clock and shift logic *)
+            sclk_reg <-- ~:(sclk_reg.value);
+            if_ (sclk_reg.value) [ (* Falling edge of SCLK *)
               shift_reg <-- (sll shift_reg.value 1); (* Shift Left *)
               bit_cnt   <-- (bit_cnt.value +:. 1);][];
           
-          if_ (bit_cnt.value ==:. 8) [
-            running <-- gnd;
+            if_ (bit_cnt.value ==:. 8) [
+              running <-- gnd;
+            ] [];
           ] [];
-        ]
-      ]);
+      ]];);
     { O.mosi = msb shift_reg.value
     ; sclk = sclk_reg.value
     ; cs = ~:(running.value)
-    ; dc = running.value 
     ; ready = ~:(running.value)
     }
 
