b5b882ec8cf81c99f5f6ce4d8b2c0cab0dc4f838
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-01-23 22:00:39 +0000

    Update simulate.ml

--- all_projects/project07/bmp280_hardcaml/bin/simulate.ml
+++ all_projects/project07/bmp280_hardcaml/bin/simulate.ml
@@ -1,1 +1,39 @@
 (* simulate.ml *)
+
+open Hardcaml
+open Hardcaml_waveterm
+open Project04_lib
+
+module My_config = struct
+  let clk_fre = 27
+  let is_simulation = true
+end
+
+module MyBmp280 = Top.Make(My_config)
+
+module Simulator = Cyclesim.With_interface (MyBmp280.I)(MyBmp280.O)
+
+let testbench n =
+  let scope = 
+    Scope.create 
+      ~auto_label_hierarchical_ports:true
+      ~flatten_design:true () in
+  let oc = open_out "bmp280.vcd" in
+  let sim = 
+    Simulator.create
+      ~config:Cyclesim.Config.trace_all (MyBmp280.create scope) |> Vcd.wrap oc in
+  let inputs : _ MyBmp280.I.t = Cyclesim.inputs sim in
+  let _outputs : _ MyBmp280.O.t = Cyclesim.outputs sim in
+  let waves, sim = Waveform.create sim in
+
+  inputs.reset := Bits.gnd;
+
+  for _i = 0 to n do
+    Cyclesim.cycle sim
+  done;
+  close_out oc;
+  waves
+
+let () =
+  let waves = testbench 1000 in
+  Hardcaml_waveterm_interactive.run ~wave_width:5 ~signals_width:30 waves
