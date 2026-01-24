5533e223ef2274821a1e1680774a406581fd002a
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2026-01-24 09:16:43 +0000

    Develop

--- all_projects/project07/bmp280_hardcaml/lib/i2c_master.ml
+++ all_projects/project07/bmp280_hardcaml/lib/i2c_master.ml
@@ -1,11 +1,14 @@
 (* i2c_master.ml *)
 open Hardcaml
 open Signal
-input clk, rst_n,
+(*
+    input clk, rst_n,
     input [6:0] addr, [7:0] reg_addr, [7:0] din,
     input rw, start,
     output reg [7:0] dout, output reg done, ack_error,
     inout sda, output scl
+*)
+
 module type Config = Config.S
 
 module Make (X : Config.S) = struct
@@ -14,9 +17,12 @@ module Make (X : Config.S) = struct
     type 'a t =
       { clock : 'a
       ; reset : 'a
-	  ; addr : 'a[@bits 6]
-	  ; reg_addr : 'a[@bits 6]
-	  ; din : 'a
+	    ; addr : 'a[@bits 7]
+	    ; reg_addr : 'a[@bits 8]
+	    ; din : 'a [@bits 8]
+      ; rw : 'a
+      ; start : 'a
+      ; sda_in : 'a
       } 
     [@@deriving hardcaml]
   end
@@ -24,24 +30,24 @@ module Make (X : Config.S) = struct
   module O = struct
     type 'a t =
       { scl : 'a
-      ; sda : 'a
-	  ; done : 'a
-	  ; ack_error : 'a
-	  ; dout : 'a[@bits76]
+      ; sda_out : 'a
+	    ; ready : 'a
+	    ; ack_error : 'a
+	    ; dout : 'a [@bits 8]
       }
     [@@deriving hardcaml]
   end
 	
-  let create (scope : Scope.t) (input : Signal.t I.t) : Signal.t O.t =
-	  let sync_spec = Reg_spec.create ~clock:input.clock ~reset:input.reset () in
-    let counter_1s = reg_fb sync_spec 
-                       ~enable:vdd 
-                       ~width:32 
-                       ~f:(fun d -> mux2 (d ==:. wait_time) (zero 32) (d +:. 1)) in
+  let create (_scope : Scope.t) (_input : Signal.t I.t) : Signal.t O.t =
+	  let _sync_spec = Reg_spec.create ~clock:_input.clock ~reset:_input.reset () in
 
     (* Return circuit output value *)
-    { O.i2c_master = i2c_master.data }  (* Placeholder for actual LED output *)
-
+    { O.scl = zero 1
+    ; O.sda_out = zero 1
+    ; O.ready = vdd
+    ; O.ack_error = gnd
+    ; O.dout = zero 8
+    }
   let hierarchical (scope : Scope.t) (i : Signal.t I.t) : Signal.t O.t =
     let module H = Hierarchy.In_scope(I)(O) in
     H.hierarchical ~scope ~name:"i2c_master" ~instance:"inst1" create i
--- all_projects/project07/bmp280_hardcaml/lib/top.ml
+++ all_projects/project07/bmp280_hardcaml/lib/top.ml
@@ -6,6 +6,11 @@ module type Config = Config.S
 
 module Make (X : Config.S) = struct
 
+  (*
+      input clk, rst_n,
+    output [19:0] raw_temp, raw_press,
+    inout sda, output scl
+  *)
   module I = struct
     type 'a t =
       { clock : 'a
@@ -24,15 +29,41 @@ module Make (X : Config.S) = struct
 
   module MyI2c_master = I2c_master.Make(X)
 	
-  let create (scope : Scope.t) (input : Signal.t I.t) : Signal.t O.t =
+  let create (_scope : Scope.t) (input : Signal.t I.t) : Signal.t O.t =
+    let open Always in
 	  let sync_spec = Reg_spec.create ~clock:input.clock ~reset:input.reset () in
-    let counter_1s = reg_fb sync_spec 
-                       ~enable:vdd 
-                       ~width:32 
-                       ~f:(fun d -> mux2 (d ==:. wait_time) (zero 32) (d +:. 1)) in
 
-    let i2c_master = MyI2c_master.hierarchical scope (
-	     MyI2c_masterI.{ reset=input.reset; clock=input.clock }) in
+    (*
+    reg [3:0] state;
+    reg start;
+    wire done;
+    reg [7:0] reg_addr, wdata;
+    *)
+    let state = Variable.reg ~enable:vdd sync_spec ~width:4 in
+    let start = Variable.reg ~enable:vdd sync_spec ~width:1 in
+    let reg_addr = Variable.reg ~enable:vdd sync_spec ~width:8 in
+    let wdata = Variable.reg ~enable:vdd sync_spec ~width:8 in
+
+    let i2c_master = MyI2c_master.hierarchical _scope (
+	     MyI2c_master.I.{ reset=input.reset; clock=input.clock }) in
+
+    (*
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) state <= 0;
+        else case (state)
+            0: begin // Step 1: Configure Sensor
+                reg_addr <= 8'hF4; wdata <= 8'h27; start <= 1;
+                if (done) state <= 1;
+            end
+            1: begin // Step 2: Request Pressure MSB (0xF7)
+                reg_addr <= 8'hF7; start <= 1;
+                if (done) state <= 2;
+            end
+            // Continue sequencing through all 6 data registers
+        endcase
+    end
+    *)
+
     (* Return circuit output value *)
     { O.leds = zero 6; O.i2c_master = i2c_master.data }  (* Placeholder for actual LED output *)
 
