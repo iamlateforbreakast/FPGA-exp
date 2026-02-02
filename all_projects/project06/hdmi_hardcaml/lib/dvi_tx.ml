1efd7b33c1648e759bce1808ab25c3eb2c60930c
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-02-02 09:38:48 +0000

    Add input and output modules for DVI TX
    Define input and output types for DVI transmitter.

--- all_projects/project06/hdmi_hardcaml/lib/dvi_tx.ml
+++ all_projects/project06/hdmi_hardcaml/lib/dvi_tx.ml
@@ -1,1 +1,31 @@
+(* dvi_tx.ml *)
+open Hardcaml
+open Signal
 
+module type Config = Config.S
+
+module Make (X : Config.S) = struct
+
+  module I = struct
+    type 'a t = {
+      rst_n        : 'a;
+      serial_clk   : 'a;
+      rgb_clk      : 'a;
+      rgb_vs       : 'a;
+      rgb_hs       : 'a;
+      rgb_de       : 'a;
+      rgb_r        : 'a; [@bits 8]
+      rgb_g        : 'a; [@bits 8]
+      rgb_b        : 'a; [@bits 8]
+    } [@@deriving sexp_of, hardcaml]
+  end
+
+  module O = struct
+    type 'a t = {
+      tmds_clk_p   : 'a;
+      tmds_clk_n   : 'a;
+      tmds_data_p  : 'a; [@bits 3]
+      tmds_data_n  : 'a; [@bits 3]
+    } [@@deriving sexp_of, hardcaml]
+  end
+end
