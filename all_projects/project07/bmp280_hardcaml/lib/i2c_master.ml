943c0ff98ff6333f45a7c2287d864d67e4e95df3
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-02-08 21:35:51 +0000

    Update i2c_master.ml

--- all_projects/project07/bmp280_hardcaml/lib/i2c_master.ml
+++ all_projects/project07/bmp280_hardcaml/lib/i2c_master.ml
@@ -87,3 +87,105 @@ Always.(compile [
     (* ... more states for ADDR, RW, ACK, DATA ... *)
   ]
 ]);
+
+(* AI *)
+open Hardcaml
+open Signal
+
+module Make (X : Config.S) = struct
+  (* ... Interfaces I and O as defined in your snippet ... *)
+
+  let create (scope : Scope.t) (input : Signal.t I.t) : Signal.t O.t =
+    let spec = Reg_spec.create ~clock:input.clock ~reset:input.reset () in
+
+    (* --- Internal Signals & Registers --- *)
+    let step_counter = reg_fb spec ~enable:vdd ~w:16 (fun fb -> fb +: 1) in
+    let bit_index = Always.Variable.reg spec ~w:3 () in
+    let shift_reg = Always.Variable.reg spec ~w:8 () in
+    
+    (* I2C Signals *)
+    let scl_o = Always.Variable.all_reg spec ~w:1 () in
+    let sda_o = Always.Variable.all_reg spec ~w:1 () in
+    let sda_oe = Always.Variable.all_reg spec ~w:1 () in
+    let ready = Always.Variable.all_reg spec ~w:1 () in
+    let ack_err = Always.Variable.all_reg spec ~w:1 () in
+
+    module State = struct
+      type t = IDLE | START | ADDR | ACK_ADDR | STOP [@@deriving sexp_of, compare, enumerate]
+    end
+    let sm = Statemachine.create (module State) spec in
+
+    (* Constant for timing: adjust based on clk frequency *)
+    let quarter_period = of_int ~w:16 250 in 
+
+    Always.(compile [
+      sm.switch [
+        IDLE, [
+          ready <-- vdd;
+          scl_o <-- vdd;
+          sda_o <-- vdd;
+          sda_oe <-- gnd;
+          if_ input.start [
+            shift_reg <-- input.addr @: input.rw; (* Concatenate addr + rw *)
+            bit_index <-- of_int ~w:3 7;
+            ready <-- gnd;
+            sm.set_next START;
+          ];
+        ];
+
+        START, [
+          sda_oe <-- vdd;
+          sda_o <-- gnd; (* SDA drop while SCL high *)
+          if_ (step_counter.value ==: quarter_period) [
+            scl_o <-- gnd;
+            sm.set_next ADDR;
+          ];
+        ];
+
+        ADDR, [
+          sda_oe <-- vdd;
+          sda_o <-- (shift_reg.value ==>: bit_index.value);
+          
+          (* Simple SCL toggling logic *)
+          if_ (step_counter.value ==: (quarter_period *: of_int ~w:16 2)) [ scl_o <-- vdd ];
+          if_ (step_counter.value ==: (quarter_period *: of_int ~w:16 4)) [
+            scl_o <-- gnd;
+            if_ (bit_index.value ==: zero 3) [
+              sm.set_next ACK_ADDR;
+            ] [
+              bit_index <-- bit_index.value -: 1;
+            ];
+          ];
+        ];
+
+        ACK_ADDR, [
+          sda_oe <-- gnd; (* Release bus for slave ACK *)
+          if_ (step_counter.value ==: (quarter_period *: of_int ~w:16 2)) [
+            scl_o <-- vdd;
+          ];
+          if_ (step_counter.value ==: (quarter_period *: of_int ~w:16 3)) [
+            (* Sample ACK: SDA should be LOW from slave *)
+            ack_err <-- input.sda_in; 
+          ];
+          if_ (step_counter.value ==: (quarter_period *: of_int ~w:16 4)) [
+            scl_o <-- gnd;
+            sm.set_next STOP;
+          ];
+        ];
+
+        STOP, [
+          sda_oe <-- vdd;
+          sda_o <-- gnd;
+          if_ (step_counter.value ==: quarter_period) [ scl_o <-- vdd ];
+          if_ (step_counter.value ==: (quarter_period *: of_int ~w:16 2)) [ 
+            sda_o <-- vdd; (* SDA rise while SCL high *)
+            sm.set_next IDLE;
+          ];
+        ];
+      ]
+    ]);
+
+    { O.scl = scl_o.value; O.sda_out = sda_o.value; O.ready = ready.value; 
+      O.ack_error = ack_err.value; O.dout = zero 8 }
+end
+
