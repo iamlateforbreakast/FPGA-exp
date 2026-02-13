6a25894941404fd1fcdf4124a341782385a04841
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-02-13 15:36:24 +0000

    Update i2c_master.ml

--- all_projects/project07/bmp280_hardcaml/lib/i2c_master.ml
+++ all_projects/project07/bmp280_hardcaml/lib/i2c_master.ml
@@ -31,6 +31,12 @@ module Make (X : Config.S) = struct
       [@@deriving hardcaml]
   end
 
+  module State = struct
+    type t = IDLE | START | ADDR | ACK_ADDR | REG_ADDR | ACK_REG 
+           | RESTART | READ_DATA | MSTR_ACK | STOP 
+           [@@deriving sexp_of, compare, enumerate]
+  end
+  
   let create (_scope : Scope.t) (input : Signal.t I.t) : Signal.t O.t =
     let half_period = quarter_period *: of_int ~w:16 2 in
     let full_period = quarter_period *: of_int ~w:16 4 in
@@ -118,8 +124,92 @@ module Make (X : Config.S) = struct
           sm.set_next IDLE;
         ];
       ];
+
+	  ACK_ADDR, [
+        sda_oe <-- gnd;
+        if_ (step_counter.value ==: half_period) [ scl_o <-- vdd ];
+        if_ (step_counter.value ==: full_period) [
+          scl_o <-- gnd;
+          step_counter <-- .zero;
+          (* If we just sent the Slave Addr, move to Reg Addr *)
+          shift_reg <-- input.reg_addr;
+          bit_index <-- of_int ~w:3 7;
+          sm.set_next REG_ADDR;
+        ];
+      ];
+
+      REG_ADDR, [
+        sda_oe <-- vdd;
+        sda_o <-- (shift_reg.value ==>: bit_index.value);
+        if_ (step_counter.value ==: half_period) [ scl_o <-- vdd ];
+        if_ (step_counter.value ==: full_period) [
+        scl_o <-- gnd;
+        step_counter <-- .zero;
+        if_ (bit_index.value ==: zero 3) [ sm.set_next ACK_REG ]
+        else_ [ bit_index <-- bit_index.value -: 1 ];
+        ];
+      ];
+
+      ACK_REG, [
+        sda_oe <-- gnd;
+        if_ (step_counter.value ==: half_period) [ scl_o <-- vdd ];
+        if_ (step_counter.value ==: full_period) [
+          scl_o <-- gnd;
+          step_counter <-- .zero;
+          (* Decide: Write data or Restart for a Read? *)
+          if_ input.rw [
+            sm.set_next RESTART;
+          ] [
+            shift_reg <-- input.din;
+            bit_index <-- of_int ~w:3 7;
+            sm.set_next WRITE_DATA; (* You can implement this similarly to REG_ADDR *)
+          ];
+        ];
+      ];
+
+      RESTART, [
+        (* Repeated Start: SDA goes high then low while SCL is high *)
+        sda_oe <-- vdd;
+        if_ (step_counter.value ==: zero 16) [ sda_o <-- vdd ];
+        if_ (step_counter.value ==: quarter_period) [ scl_o <-- vdd ];
+        if_ (step_counter.value ==: half_period) [ sda_o <-- gnd ]; (* Start condition *)
+        if_ (step_counter.value ==: (quarter_period *: of_int ~w:16 3)) [
+          scl_o <-- gnd;
+          shift_reg <-- input.addr @: vdd; (* Address + READ bit *)
+          bit_index <-- of_int ~w:3 7;
+          step_counter <-- .zero;
+          sm.set_next ADDR_READ; (* New state to handle the second address phase *)
+        ];
+      ];
+
+      READ_DATA, [
+        sda_oe <-- gnd; (* Release SDA for slave to drive data *)
+        if_ (step_counter.value ==: half_period) [
+          scl_o <-- vdd;
+          (* Sample data at the middle of the high SCL pulse *)
+          shift_reg <-- insert ~into:shift_reg.value ~at:bit_index.value input.sda_in;
+        ];
+        if_ (step_counter.value ==: full_period) [
+          scl_o <-- gnd;
+          step_counter <-- .zero;
+          if_ (bit_index.value ==: zero 3) [ sm.set_next MSTR_ACK ]
+          else_ [ bit_index <-- bit_index.value -: 1 ];
+        ];
+      ];
+
+      MSTR_ACK, [
+        sda_oe <-- vdd;
+        (* For a single byte read, send NACK (SDA High) to end *)
+        sda_o <-- vdd; 
+        if_ (step_counter.value ==: half_period) [ scl_o <-- vdd ];
+        if_ (step_counter.value ==: full_period) [
+          scl_o <-- gnd;
+          step_counter <-- .zero;
+          sm.set_next STOP;
+        ];
+      ];
     ]
-  ]);
+    ]);
     (* Return circuit output value *)
     { O.scl = scl_o.value; O.sda_out = sda_o.value; O.ready = ready.value; 
       O.ack_error = ack_err.value; O.dout = zero 8 }
